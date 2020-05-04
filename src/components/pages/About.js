c455e2846a4fdcc3dcf663c1dfcdf542724b6151
David Stevic <dacha.stevic@gmail.com>
2020-05-05 00:33:06 +0200

    promene oko slike i teksta kod about stranice

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,9 +6,9 @@ function About() {
     return (
         <React.Fragment >
             <div>
-                <div className="firstContainter">
-                    <img src={Slike} alt="" style={bckImg}/>
-                    <h1 style={firstLine}>About To Do List App</h1>
+                <div className='firstContainter'>
+                    <img src={Slike} alt="" className='bckImg'/>
+                    <h1 className='firstLine'>About To Do List App</h1>
                 </div>
                 <div>
                     <p>Thsi is my first app in react</p>
@@ -18,21 +18,5 @@ function About() {
     )
 }
 
-const firstLine={
-    textAlign: 'center',
-    padding: '50px',
-    textShadow: 'rgba(0, 0, 0, 0.7) 0px 0px 11px',
-    fontSize: '50px',
-    position: 'absolute',
-    left: '55%',
-    top: '20%',
-}
-
-const bckImg={
-    display: 'flex',
-    width: '800px',
-    padding: '50px 50px',
-    position: 'relativ',
-}
 
 export default About;
